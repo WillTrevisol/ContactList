68c035ddcaaf078c981533a26735772331d487c3
Willian Trevisol <trevisolwillian@gmail.com>
2025-09-13 19:29:19 -0300

    feat: add contact activity

---
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/ContactActivity.kt
@@ -0,0 +1,50 @@
+package br.edu.scl.ifsp.sdm.contactlist.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.scl.ifsp.sdm.contactlist.R
+import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityContactBinding
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_CONTACT
+import br.edu.scl.ifsp.sdm.contactlist.model.Contact
+
+class ContactActivity : AppCompatActivity() {
+
+    private val acb: ActivityContactBinding by lazy {
+        ActivityContactBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(acb.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setSupportActionBar(acb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = getString(R.string.contact_details)
+
+        with(acb) {
+            saveButton.setOnClickListener {
+                val contact = Contact(
+                    id = hashCode(),
+                    name = nameEditText.text.toString(),
+                    phone = phoneEditText.text.toString(),
+                    email = emailEditText.text.toString(),
+                    address = addressEditText.text.toString(),
+                )
+
+                val resultIntent = Intent()
+                resultIntent.putExtra(EXTRA_CONTACT, contact)
+                setResult(RESULT_OK, resultIntent)
+                finish()
+            }
+        }
+    }
+}
--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
@@ -1,9 +1,17 @@
 package br.edu.scl.ifsp.sdm.contactlist.view
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ArrayAdapter
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import br.edu.scl.ifsp.sdm.contactlist.R
 import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityMainBinding
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_CONTACT
 import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 
 class MainActivity : AppCompatActivity() {
@@ -19,17 +27,54 @@ class MainActivity : AppCompatActivity() {
         ArrayAdapter(this, android.R.layout.simple_list_item_1, contactList.map { it.toString() })
     }
 
+    private lateinit var contactActivityResultLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        setSupportActionBar(amb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = getString(R.string.contact_list)
+
+        contactActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val contact = result.data?.getParcelableExtra<Contact>(EXTRA_CONTACT)
+                contact?.also { contact ->
+                    if (contactList.any { it.id == contact.id }) {
+                        // Edit
+                    } else {
+                        contactList.add(contact)
+                        contactAdapter.add(contact.toString())
+                    }
+                }
+                contactAdapter.notifyDataSetChanged()
+            }
+        }
 
         fillContacts()
 
         amb.contactsListView.adapter = contactAdapter
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when(item.itemId) {
+            R.id.addContactMenuItem -> {
+                contactActivityResultLauncher.launch(Intent(this, ContactActivity::class.java))
+                true
+            }
+
+            else -> { false }
+        }
+    }
+
     private fun fillContacts() {
-        for (i in 1..50) {
+        for (i in 1..10) {
             contactList.add(
                 Contact(
                     id = i,
