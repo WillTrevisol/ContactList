683eb705b5a1fd90d2b7b8bc1c0a8b832bfb9c6d
Willian Trevisol <trevisolwillian@gmail.com>
2025-09-13 18:07:05 -0300

    feat: add contact model, adapter and populating list with fake data

--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
@@ -2,15 +2,43 @@ package br.edu.scl.ifsp.sdm.contactlist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityMainBinding
+import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    // Data source
+    private val contactList: MutableList<Contact> = mutableListOf()
+
+    // Adapter
+    private val contactAdapter: ArrayAdapter<String> by lazy {
+        ArrayAdapter(this, android.R.layout.simple_list_item_1, contactList.map { it.toString() })
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        fillContacts()
+
+        amb.contactsListView.adapter = contactAdapter
+    }
+
+    private fun fillContacts() {
+        for (i in 1..50) {
+            contactList.add(
+                Contact(
+                    id = i,
+                    name = "Nome $i",
+                    address = "Endereço $i",
+                    phone = "Telefone $i",
+                    email = "Email $i"
+                )
+            )
+        }
     }
 }
