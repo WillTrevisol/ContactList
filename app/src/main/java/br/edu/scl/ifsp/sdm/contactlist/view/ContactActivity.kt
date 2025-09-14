5132e10ee9282e787700efd970af71fb7f631c06
Willian Trevisol <trevisolwillian@gmail.com>
2025-09-14 17:22:30 -0300

    feat: add view, edit and remove contact features

--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/ContactActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/ContactActivity.kt
@@ -2,13 +2,16 @@ package br.edu.scl.ifsp.sdm.contactlist.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View.GONE
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.transition.Visibility
 import br.edu.scl.ifsp.sdm.contactlist.R
 import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityContactBinding
 import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_CONTACT
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_VIEW_CONTACT
 import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 
 class ContactActivity : AppCompatActivity() {
@@ -30,10 +33,29 @@ class ContactActivity : AppCompatActivity() {
         setSupportActionBar(acb.toolbarIn.toolbar)
         supportActionBar?.subtitle = getString(R.string.contact_details)
 
+        val receivedContact = intent.getParcelableExtra<Contact?>(EXTRA_CONTACT)
+        receivedContact?.let {
+            val viewContact = intent.getBooleanExtra(EXTRA_VIEW_CONTACT, false)
+            with(acb) {
+                if (viewContact) {
+                    nameEditText.isEnabled = false
+                    phoneEditText.isEnabled = false
+                    emailEditText.isEnabled = false
+                    addressEditText.isEnabled = false
+                    saveButton.visibility = GONE
+                }
+
+                nameEditText.setText(it.name)
+                phoneEditText.setText(it.phone)
+                emailEditText.setText(it.email)
+                addressEditText.setText(it.address)
+            }
+        }
+
         with(acb) {
             saveButton.setOnClickListener {
                 val contact = Contact(
-                    id = hashCode(),
+                    id = receivedContact?.id ?: hashCode(),
                     name = nameEditText.text.toString(),
                     phone = phoneEditText.text.toString(),
                     email = emailEditText.text.toString(),
@@ -46,5 +68,7 @@ class ContactActivity : AppCompatActivity() {
                 finish()
             }
         }
+
+
     }
 }
--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
@@ -3,15 +3,20 @@ package br.edu.scl.ifsp.sdm.contactlist.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ArrayAdapter
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.scl.ifsp.sdm.contactlist.R
 import br.edu.scl.ifsp.sdm.contactlist.adapter.ContactAdapter
 import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityMainBinding
 import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_CONTACT
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_VIEW_CONTACT
 import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 
 class MainActivity : AppCompatActivity() {
@@ -39,11 +44,12 @@ class MainActivity : AppCompatActivity() {
         contactActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_OK) {
                 val contact = result.data?.getParcelableExtra<Contact>(EXTRA_CONTACT)
-                contact?.also { contact ->
-                    if (contactList.any { it.id == contact.id }) {
-                        // Edit
+                contact?.also { receivedContact ->
+                    if (contactList.any { it.id == receivedContact.id }) {
+                        val position = contactList.indexOfFirst{it.id == receivedContact.id}
+                        contactList[position] = receivedContact
                     } else {
-                        contactList.add(contact)
+                        contactList.add(receivedContact)
                     }
                 }
                 contactAdapter.notifyDataSetChanged()
@@ -53,6 +59,42 @@ class MainActivity : AppCompatActivity() {
         fillContacts()
 
         amb.contactsListView.adapter = contactAdapter
+
+        registerForContextMenu(amb.contactsListView)
+
+        amb.contactsListView.onItemClickListener = OnItemClickListener { parent, view, position, id ->
+            startActivity(Intent(this, ContactActivity::class.java).apply {
+                putExtra(EXTRA_CONTACT, contactList[position])
+                putExtra(EXTRA_VIEW_CONTACT, true)
+            })
+        }
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menuInflater.inflate(R.menu.context_menu_main, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val position = (item.menuInfo as AdapterView.AdapterContextMenuInfo).position
+        return when (item.itemId) {
+            R.id.removeContactMenuItem -> {
+                contactList.removeAt(position)
+                contactAdapter.notifyDataSetChanged()
+                Toast.makeText(this, getString(R.string.contact_removed), Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.editContactMenuItem -> {
+                contactActivityResultLauncher.launch(Intent(this, ContactActivity::class.java).apply {
+                    putExtra(EXTRA_CONTACT, contactList[position])
+                })
+                true
+            }
+            else -> { false }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -61,7 +103,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
         return when(item.itemId) {
             R.id.addContactMenuItem -> {
                 contactActivityResultLauncher.launch(Intent(this, ContactActivity::class.java))
@@ -70,6 +111,11 @@ class MainActivity : AppCompatActivity() {
 
             else -> { false }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterForContextMenu(amb.contactsListView)
     }
 
     private fun fillContacts() {
